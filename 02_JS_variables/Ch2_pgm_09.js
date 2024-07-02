1d0c1d9bdb0475540daef85d8aa639f687b033a0
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-02 21:48:19 +0530

    Update Ch2_pgm_09.js

--- 02_JS_variables/Ch2_pgm_09.js
+++ 02_JS_variables/Ch2_pgm_09.js
@@ -6,12 +6,14 @@ var _underscore56;
 var StartWithCapital;
 var z5;
 
-var 999;
-var 39Steps;
-var &nope;
-var single words only;
+var 999;//Number cannot be the variable name
+var 39Steps;//number should not be placed in the starting of the variable name
+var &nope;//The special character & cannot be the variiable name
+var single words only;//The variable name should not contain a white space
 var yield;
-
+//The vaiable name can only start with a letter or the special character  $ or _ ,
+it can have numbers but should not start with a number,
+  the variable name should not contain white space we can specify that with a underscore(-)
 
 
 /* Further Adventures
