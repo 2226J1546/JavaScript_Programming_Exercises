de672bc3d5d8ebe7ddb50ec0c2054021066f8c20
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-03 20:09:55 +0530

    Update Ch4_pgm_08.js

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -7,10 +7,21 @@ displayMenu = function () {
 	console.log("(1) Print log");
 	console.log("(2) Upload file");
 	console.log("(9) Quit");
+	console.log("(5) Save");
+	console.log("(7) Open");
 };
 
 displayMenu();
-
+ var firstone;
+firstone=function(){
+	console.log("Save");
+	console.log("Save as");
+	console.log("Open");
+	console.log("Edit");
+	console.log("Create");
+	console.log("Tools");
+};
+firstone();
 
 
 /* Further Adventures
