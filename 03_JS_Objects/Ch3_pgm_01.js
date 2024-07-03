a919fe79e8175ebaba059a726063d8f3cf8b85f3
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-03 09:19:14 +0530

    Update Ch3_pgm_01.js

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -8,6 +8,17 @@ bookAuthor = "J. R. R. Tolkien";
 
 console.log(bookTitle + " by " + bookAuthor);
 
+var book1Title = "The Hobbit";
+var book1Author = "J.R.R.Tolkien";
+
+var book2Title = "Northern Lights";
+var book2Author = "Philip Pullman";
+
+
+console.log("There are three books so far...");
+console.log(book1Title + " by " + book1Author);
+console.log(book2Title + " by " + book2Author);
+
 
 
 /* Further Adventures
