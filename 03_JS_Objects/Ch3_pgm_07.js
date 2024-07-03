3e93394793c79cfdc1b70d8ffc683b86aeda8788
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-03 09:32:36 +0530

    Update Ch3_pgm_07.js

--- 03_JS_Objects/Ch3_pgm_07.js
+++ 03_JS_Objects/Ch3_pgm_07.js
@@ -2,17 +2,18 @@
 
 var book1;
 var book2;
-
+var book3;
 book1 = {
     title : "The Hobbit",
-    author : "J. R. R. Tolkien"
+    author : "J. R. R. Tolkien";
+    year:"1976";
 };
 
 book2 = {
 	title : "Northern Lights",
-	author : "Philip Pullman"
+	author : "Philip Pullman";
+	 year:"1976";
 };
-
 
 
 /* Further Adventures
