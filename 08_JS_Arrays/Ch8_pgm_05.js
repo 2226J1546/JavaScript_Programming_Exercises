937af7cc22c5f9dda3bbdd5af4b7360b9a7bba3c
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-13 11:15:46 +0530

    Update Ch8_pgm_05.js

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -21,13 +21,48 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
 	return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
-
+var visitors = [
+	[ 354, 132, 210, 221, 481,345,678 ]
+  [100, 120, 110, 105, 130, 140, 125], 
+  [115, 118, 122, 128, 135, 130, 140], 
+  [105, 112, 115, 120, 125, 130, 135], 
+[100, 120, 110, 105, 130, 140, 125], 
+  [115, 118, 122, 128, 135, 130, 140], 
+  [105, 112, 115, 120, 125, 130, 135], 
+];
 var report = getVisitorReport(visitors, 2);
 
 console.log(report);
-
-
+function days(one,two,three){
+  return two,one,three
+}
+var day=["mon","tue","wed","thur","fri","sat","sun"]
+dayinm=day[2];
+  var visitors = [
+    [ 354, 132, 210, 221, 481,345,678 ]
+    [100, 120, 110, 105, 130, 140, 125], 
+    [115, 118, 122, 128, 135, 130, 140], 
+    [105, 112, 115, 120, 125, 130, 135], 
+  [100, 120, 110, 105, 130, 140, 125], 
+    [115, 118, 122, 128, 135, 130, 140], 
+    [105, 112, 115, 120, 125, 130, 135], 
+  ];
+  numin=visitors[2][3];
+  let print=days(day,visitors,2)
+console.log(`The day ${dayinm},The visitors ${numin},three`)	
+function days(one,two)
+{
+   return one,two
+}
+let week1=[105, 112, 115, 120, 125, 130, 135]
+let week2= [115, 118, 122, 128, 135, 130, 140]
+let week3= [100, 120, 110, 105, 130, 140, 125]
+let week4=[ 354, 132, 210, 221, 481,345,678 ]
+let montharr=[week1,week2,week3,week4]
+let day=prompt("Enter a day:")
+let week=montharr[2][4]
+let res=days(day,montharr)
+console.log(`The day ${day},The visitors in a week ${week}`)
 
 /* Further Adventures
  *
