70f62e66e38ac96f290dd965ce582f52c365c17e
2226J1546 <144420475+2226J1546@users.noreply.github.com>
2024-07-13 16:46:49 +0530

    Update Ch10_pgm_05.js

--- 10_JS_Conditions/Ch10_pgm_05.js
+++ 10_JS_Conditions/Ch10_pgm_05.js
@@ -4,13 +4,13 @@ var getGuesser = function () {
     var secret = Math.floor(Math.random() * 10 + 1);
   
     return function (userNumber) {
-      if (userNumber === secret) {
-        return "Well done!";
+      if (userNumber < secret) {
+        return "Too low!";
       } else {
-        if (userNumber > secret) {
-          return "Too high!";
+        if (userNumber === secret) {
+          return "Well done!";
         } else {
-          return "Too low!";
+          return "Too high!";
         }
       }
     };
